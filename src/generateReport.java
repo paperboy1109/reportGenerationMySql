f28bc4b1f2965ab70c2f883103d82bd96c871e9a
Daniel Janiak <Daniel Janiak>
2016-04-19 16:16:39 -0600

    Produce complete output, except for page breaks

--- src/generateReport.java
+++ src/generateReport.java
@@ -87,7 +87,8 @@ public class generateReport {
 			
 			//printHeader();
 			//displayAll(myRs);
-			displayCourseInfo(myRs);
+			//displayCourseInfo(myRs);
+			createReportDocument(myRs);
 
 
 		}
@@ -176,6 +177,125 @@ public class generateReport {
 			}
 			
 		}
+	}
+	
+	
+	private static void createReportDocument(ResultSet myRs) throws SQLException {
+		
+		System.out.printf(" createReportDocument called \n");
+		
+		int pageCount = 0;
+		int lineCount = 0;
+		int currentCRN = 0;
+		String currentCRN_str = "";
+	
+
+
+		while (myRs.next()) {
+			
+			int courseCRN = myRs.getInt("underEnrolledCRN");
+			int noteNumber = myRs.getInt("notenumber");
+			String courseNote = myRs.getString("note");
+			
+			String courseSubject = myRs.getString("subject");
+			int courseNumber = myRs.getInt("number");
+			int courseSection = myRs.getInt("section");
+			String courseTitle = myRs.getString("title");
+			int studentCount = myRs.getInt("NumberOfStudents");
+			
+
+			
+			// Append the information from the result set to the output file
+			String courseCRN_str = String.valueOf(courseCRN);
+			String courseSection_str = String.valueOf(courseSection);
+			String studentCount_str = String.valueOf(studentCount);
+			String courseNumber_str = String.valueOf(courseNumber);
+			String courseNote_str = String.valueOf(courseNote);
+
+			
+			try {
+				
+				//System.out.println("Trying to write file ...");
+				BufferedWriter writer = new BufferedWriter(new FileWriter(new File("reportDocument.doc"), true));
+
+				//writer.write(courseCRN_str);
+				//writer.write("\t");
+				//writer.write(courseSection_str);
+				//writer.write("\t");
+				//writer.write(studentCount_str);
+				//writer.newLine();
+				//writer.close();
+				//System.out.println("End");
+				
+				if (courseCRN!=currentCRN) {
+					
+					//System.out.printf("\n  CRN: %d, Number of students enrolled: %d\n", courseCRN, studentCount);
+					//System.out.printf("  %s %d %s \t(Section %d)\n", courseSubject, courseNumber, courseTitle, courseSection);
+					//System.out.printf("\t Notes: ");
+			
+					
+					//writer.newLine();
+					//writer.write("*** Here's the good stuff ***");
+					
+					writer.write("\n  CRN: " + courseCRN_str + ", Number of students enrolled: " + studentCount_str + "\n");
+					writer.write("  " + courseSubject + " ");
+					writer.write(courseNumber_str);
+					writer.write(" " + courseTitle);
+					writer.write("\t (Section " + courseSection_str + ")\n");
+					writer.write("\tNotes: ");
+					
+					courseCRN_str = currentCRN_str;
+				}
+				
+				
+				if (courseNote == null) {
+					//System.out.printf("none\n");
+					writer.write("none\n");
+				} else if (noteNumber != 1) {
+					//System.out.printf("\t");
+					//System.out.printf(" %s \n", courseNote);
+					writer.write("\t");
+					
+					//writer.write("hello ");
+					
+					writer.write(courseNote + "\n");
+				} else {
+					//System.out.printf(" %s \n", courseNote);
+					//writer.write("hello I'm a new note");
+					
+					writer.write(courseNote + "\n");
+				}
+				
+				//writer.write("\n___ end good stuff ___ \n");
+				writer.close();
+
+
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+			
+
+			
+			if (courseCRN!=currentCRN) {
+				System.out.printf("\n  CRN: %d, Number of students enrolled: %d\n", courseCRN, studentCount);
+				System.out.printf("  %s %d %s \t(Section %d)\n", courseSubject, courseNumber, courseTitle, courseSection);
+				System.out.printf("\t Notes: ");
+		
+				currentCRN = courseCRN;
+			}
+
+			//System.out.printf("\t Note: %d, %s \n", noteNumber, courseNote);
+			if (courseNote == null) {
+				System.out.printf("none\n");
+			} else if (noteNumber != 1) {
+				System.out.printf("\t");
+				System.out.printf(" %s \n", courseNote);
+			} else {
+			System.out.printf(" %s \n", courseNote);
+			}
+		} //while
+		
+
 	}
 	
 	
